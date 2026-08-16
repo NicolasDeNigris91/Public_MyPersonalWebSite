7aa795c763c74bf02720ecd7a7630d0f75efcfbb
Nicolas Pilegi De Nigris <nicolas.denigris91@icloud.com>
2026-08-16 18:50:26 -0300

    ci: relax commitlint for dependabot and add auto-approve for patch (#41)
    * ci: relax commitlint for dependabot and add auto-approve for patch
    
    * ci: scope contents:write to job for dependabot auto-approve

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,8 +1,9 @@
 /** @type {import('@commitlint/types').UserConfig} */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  ignores: [(msg) => /Signed-off-by: dependabot\[bot\]/.test(msg)],
   rules: {
-    'header-max-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 200],
     'subject-case': [0],
     'body-max-line-length': [0],
     'footer-max-line-length': [0],
